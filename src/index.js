39659879886956f31fd7cf8261d5788ba29cccef
Alexey <41744678+Kristalys@users.noreply.github.com>
2018-10-03 16:16:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,25 +19,3 @@ module.exports = function getZerosCount(number, base) {
   }
   return z;
 };
-
-/*
-let p;  let c;  let k;  let zeroesCount;  zeroesCount = number;
-  for (i = 2; i <= base; i++) {
-    if (base % i == 0) {
-      p = 0;
-      while (base % i == 0) {
-        p++;
-        base = Math.floor(base / i);
-      }
-      c = 0;
-      k = number;
-      while (k / i > 0) {
-        c += Math.floor(k / i);
-        k = Math.floor(k / i);
-      }
-      zeroesCount = Math.min(zeroesCount, Math.floor(c / p));
-    }
-  }
-  return zeroesCount;
-};
-*/
